dded5cc99be9a2a6aeda9cfdd2e58ff207a7f752
manderz11 <56352368+manderz11@users.noreply.github.com>
2023-05-18 20:13:37 +0300

    Update for EXILED 7.0.0

--- BringBackPinkCandy/PinkCandyPlugin.cs
+++ BringBackPinkCandy/PinkCandyPlugin.cs
@@ -7,7 +7,7 @@ namespace BringBackPinkCandy
     {
         public override string Name => "Bring back pink candy";
         public override string Author => "manderz11";
-        public override Version Version => new Version(1, 0, 0);
+        public override Version Version => new Version(1, 0, 1);
         private CandyGrabHandle candyGrabHandle;
         private static readonly PinkCandyPlugin Singleton = new();
 
